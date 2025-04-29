398c09db6fd6c12a1817dee64bba488478072198
Van Ma <117691848+Van-Ma@users.noreply.github.com>
2025-04-28 22:15:16 -0500

    Create PbCafeAssets.js

---
+++ Components/PbCafe/PbCafeAssets.js
@@ -0,0 +1,379 @@
+export const menuItems = [
+
+    {
+        id: "1",
+        name: 'Plain',
+        description: 'Classic buttery biscuits with a golden, flaky crust.',
+        cost: 2.00,
+        img: 'PbCafe/biscuit.png',
+        type: 'Biscuits',
+        promoted: true
+      },
+      {
+        id: "2",
+        name: 'Brown Sugar',
+        description: 'A gooey brown sugar center with butter.',
+        cost: 3.00,
+        img: 'PbCafe/brownsug.png',
+        type: 'Biscuits',
+        promoted: true
+      },
+      {
+        id: "3",
+        name: 'Honey',
+        description: 'Glazed with pure local honey and butter.',
+        cost: 3.99,
+        img: 'PbCafe/honey.png',
+        type: 'Biscuits',
+        promoted: false
+      },
+      {
+        id: "4",
+        name: 'Strawberry Jam',
+        description: 'Filled with fresh strawberry jam and butter.',
+        cost: 3.00,
+        img: 'PbCafe/jamBis.png',
+        type: 'Biscuits',
+        promoted: false
+      },
+      {
+        id: "5",
+        name: 'Blueberry Jam',
+        description: 'Sweet house-made blueberry jam and butter.',
+        cost: 3.00,
+        img: 'PbCafe/blueberryBis.png',
+        type: 'Biscuits',
+        promoted: false
+      },
+      {
+        id: "6",
+        name: 'Bacon and Egg',
+        description: 'Thick country bacon, melted cheddar cheese, and a perfectly cooked egg.',
+        cost: 4.99,
+        img: 'PbCafe/bacon.png',
+        type: 'Biscuits',
+        promoted: false
+      },
+
+
+
+      {
+        id: "7",
+        name: 'Berry Bliss',
+        description: 'A mix of pressed strawberries, blueberries, and raspberries.',
+        cost: 3.50,
+        img: 'PbCafe/berryJuice.png',
+        type: 'Juice',
+        promoted: false
+      },
+      {
+        id: "8",
+        name: 'Cucumber Mint Limeade',
+        description: 'Crisp cucumber juice with a hint of mint, mixed into limeade.',
+        cost: 3.00,
+        img: 'PbCafe/cucumberJuice.png',
+        type: 'Juice',
+        promoted: false
+      },
+      {
+        id: "9",
+        name: 'Golden Kiwi Nectar',
+        description: 'Freshly pressed golden kiwi juice.',
+        cost: 3.99,
+        img: 'PbCafe/kiwi.png',
+        type: 'Juice',
+        promoted: false
+      },
+      {
+        id: "10",
+        name: 'Lemonade',
+        description: 'Freshly squeezed lemonade, perfectly balanced between tart and sweet.',
+        cost: 2.50,
+        img: 'PbCafe/lemonade.png',
+        type: 'Juice',
+        promoted: true
+      },
+      {
+        id: "11",
+        name: 'Limeade',
+        description: 'Freshly squeezed limeade, offering a zesty and refreshing flavor.',
+        cost: 2.75,
+        img: 'PbCafe/lime.png',
+        type: 'Juice',
+        promoted: false
+      },
+      {
+        id: "12",
+        name: 'Peach Perfection',
+        description: 'Pressed peach juice.',
+        cost: 3.00,
+        img: 'PbCafe/peachJuice.png',
+        type: 'Juice',
+        promoted: true
+      },
+      {
+        id: "13",
+        name: 'Pineapple Mango Fusion',
+        description: 'A tropical mix of pressed pineapple and mango juice.',
+        cost: 3.50,
+        img: 'PbCafe/pineappleJuice.png',
+        type: 'Juice',
+        promoted: true
+      },
+      {
+        id: "14",
+        name: 'Strawberry Basil Lemonade',
+        description: 'Strawberry puree blended with muddled basil and fresh strawberry slices, mixed with lemonade.',
+        cost: 3.50,
+        img: 'PbCafe/strawberry.png',
+        type: 'Juice',
+        promoted: true
+      },
+
+
+
+      {
+        id: "15",
+        name: 'Frozen Strawberry Lemonade',
+        description: 'Blended strawberries with frozen lemonade.',
+        cost: 4.25,
+        img: 'PbCafe/strawberrySmoothie.png',
+        type: 'Smoothies',
+        promoted: true
+      },
+      {
+        id: "16",
+        name: 'Frozen Kiwi Twist',
+        description: 'A blend of frozen green and golden kiwis.',
+        cost: 4.40,
+        img: 'PbCafe/d.png',
+        type: 'Smoothies',
+        promoted: false
+      },
+      {
+        id: "17",
+        name: 'Orange Dream',
+        description: 'Juicy mandarin oranges with vanilla cream.',
+        cost: 4.50,
+        img: 'PbCafe/orangeSmoothie.png',
+        type: 'Smoothies',
+        promoted: true
+      },
+      {
+        id: "18",
+        name: 'Frozen Berry',
+        description: 'Blended mix of raspberries, strawberries, and blueberries.',
+        cost: 4.35,
+        img: 'PbCafe/berrySmoothie.png',
+        type: 'Smoothies',
+        promoted: false
+      },
+      {
+        id: "19",
+        name: 'Frozen Peach',
+        description: 'Blended juicy peaches with ice.',
+        cost: 4.10,
+        img: 'PbCafe/peach.png',
+        type: 'Smoothies',
+        promoted: false
+      },
+      {
+        id: "20",
+        name: 'Frozen Pineapple Mango',
+        description: 'A chilled blend of pineapple and mango.',
+        cost: 4.25,
+        img: 'PbCafe/pineapple.png',
+        type: 'Smoothies',
+        promoted: false
+      },
+     
+      {
+        id: "21",
+        name: 'Frozen Lemonade',
+        description: 'A frozen version of our classic lemonade.',
+        cost: 4.00,
+        img: 'PbCafe/lemonadeSmoothie.png',
+        type: 'Smoothies',
+        promoted: false
+      },
+     
+
+
+
+
+      {
+        id: "22",
+        name: 'Iced Americano',
+        description: 'Strong espresso over ice with vanilla foam.',
+        cost: 4.50,
+        img: 'PbCafe/darkIce.png',
+        type: 'Coffee',
+        promoted: false
+      },
+      {
+        id: "23",
+        name: 'Cold Foam',
+        description: 'Classic cold brew topped with vanilla foam.',
+        cost: 4.50,
+        img: 'PbCafe/darkIceFoam.png',
+        type: 'Coffee',
+        promoted: false
+      },
+      {
+        id: "24",
+        name: 'Blonde',
+        description: 'Classic blonde roasted cold brew topped with vanilla foam.',
+        cost: 4.60,
+        img: 'PbCafe/blondeFoam.png',
+        type: 'Coffee',
+        promoted: true
+      },
+      {
+        id: "25",
+        name: 'Cream and Honey',
+        description: 'A smooth and rich cold brew sweetened with cream and honey.',
+        cost: 4.70,
+        img: 'PbCafe/chai.png',
+        type: 'Coffee',
+        promoted: false
+      },
+      {
+        id: "26",
+        name: 'Vanilla Latte',
+        description: 'Creamy vanilla latte topped with a frothy milk foam.',
+        cost: 4.50,
+        img: 'PbCafe/vanilla.png',
+        type: 'Coffee',
+        promoted: true
+      },
+      {
+        id: "27",
+        name: 'Mocha Latte',
+        description: 'A decadent mocha latte finished with a velvety milk foam.',
+        cost: 4.60,
+        img: 'PbCafe/mocha.png',
+        type: 'Coffee',
+        promoted: false
+      },
+      {
+        id: "28",
+        name: 'Caramel Latte',
+        description: 'Sweet caramel latte topped with milk foam.',
+        cost: 4.60,
+        img: 'PbCafe/caramel.png',
+        type: 'Coffee',
+        promoted: true
+      },
+      {
+        id: "29",
+        name: 'Vanilla Frappé',
+        description: 'A creamy blend of rich cold brew coffee, silky milk foam, and a hint of sweetness.',
+        cost: 4.60,
+        img: 'PbCafe/blondeLatte.png',
+        type: 'Coffee',
+        promoted: true
+      },
+      {
+        id: "30",
+        name: 'Mocha Frappé',
+        description: 'A creamy blend of rich mocha cold brew coffee, silky milk foam, and a hint of sweetness.',
+        cost: 4.60,
+        img: 'PbCafe/darkLatte.png',
+        type: 'Coffee',
+        promoted: true
+      },
+
+
+
+
+      {
+        id: "31",
+        name: 'Hibiscus Citrus',
+        description: 'Hibiscus flower and orange blend with fresh lime.',
+        cost: 3.50,
+        img: 'PbCafe/hibiscus.png',
+        type: 'Tea',
+        promoted: false
+      },
+      {
+        id: "32",
+        name: 'Orange Sweet Tea',
+        description: 'Sweet tea infused with fresh orange slices.',
+        cost: 3.25,
+        img: 'PbCafe/orangeTea.png',
+        type: 'Tea',
+        promoted: false
+      },
+      {
+        id: "33",
+        name: 'Peach Serenity',
+        description: 'Peach juice mixed with your choice of green or black tea.',
+        cost: 3.40,
+        img: 'PbCafe/peachJuice.png',
+        type: 'Tea',
+        promoted: false
+      },
+      {
+        id: "34",
+        name: 'Green Tea Classic',
+        description: 'A pure and refreshing green tea blend.',
+        cost: 3.10,
+        img: 'PbCafe/green tea.png',
+        type: 'Tea',
+        promoted: false
+      },
+      {
+        id: "35",
+        name: 'Sweet Tea',
+        description: 'Classic sweetened black tea with a hint of fresh lemon.',
+        cost: 2.95,
+        img: 'PbCafe/sweet.png',
+        type: 'Tea',
+        promoted: false
+      },
+      {
+        id: "36",
+        name: 'Black Tea',
+        description: 'A robust and pure black tea blend.',
+        cost: 2.90,
+        img: 'PbCafe/darkIce.png',
+        type: 'Tea',
+        promoted: false
+      },
+      {
+        id: "37",
+        name: 'Chai Tea',
+        description: 'A spiced blend of black tea with aromatic spices and a touch of sweetness.',
+        cost: 3.35,
+        img: 'PbCafe/chai.png',
+        type: 'Tea',
+        promoted: false
+      },
+      {
+        id: "38",
+        name: 'Matcha Latte',
+        description: 'Creamy blend of vibrant Matcha green tea with steamed milk.',
+        cost: 4.00,
+        img: 'PbCafe/matchalatte.png',
+        type: 'Tea',
+        promoted: false
+      },
+
+      
+      
+];
+
+export const promoImages = [
+    { src: 'PbCafe/PromoPineapple.png',
+      type: 'Juice'
+    },
+    { src: 'PbCafe/PromoLemon.png',
+        type:'Juice'
+     },
+    { src: 'PbCafe/PromoStrawberry.png',
+        type: 'Juice'
+     },
+    { src: 'PbCafe/PromoCoffee.png',
+        type:'Coffee'
+     }
+];
