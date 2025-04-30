3da169abdabd58923e871b5f5cded0611c8d30fd
Van Ma <117691848+Van-Ma@users.noreply.github.com>
2025-04-30 13:57:25 -0500

    Create InteractiveInfo.js

---
+++ Components/InteractiveInfo.js
@@ -0,0 +1,49 @@
+const InteractiveInfo = [
+    {
+        name: 'Tile Dropper', 
+        description: 'Drag and Drop Carousel',
+        file: 'openTileDropperTab', 
+        img: 'defaults/tiledropper.png', 
+        route:'tiledropper-demo',
+        type:'img',
+        category: 'interactive',
+        mobile: 'true',
+    },
+
+     {
+        name: "Pea Run",
+        description: 'Playable Character Animation',
+        tab:"openPeaRunTab",
+        route: "pearun-demo",
+        img: "defaults/prun.png",
+        type:'img',
+        category: 'interactive',
+        mobile: 'false',
+    },
+
+    {
+        name: 'Toggle Theme', 
+        description: 'Color Shifting Button',
+        file: 'openToggleTab', 
+        img: 'defaults/toggle.png', 
+        route:'togglebutton-demo',
+        type:'img',
+        category: 'interactive',
+        mobile: 'true',
+    },
+
+
+    {
+        name: "Fish Bowl",
+        description:"Interactive Mouse Tracking Visual",
+        tab:"openFishTab",
+        route: "fish-demo",
+        img: "defaults/fish.png",
+        type:'img',
+        category: 'interactive',
+        mobile: 'false',
+    },
+
+];
+
+export default InteractiveInfo;
